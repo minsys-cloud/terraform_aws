b5e7fc2aa4d3529c3a7d3fbf47899fe0fab59683
lvi <lvi@minsys.io>
2019-03-09 02:31:25 -0500

    updated(test2) - security groups

--- tests/z02/security_groups.tf
+++ tests/z02/security_groups.tf
@@ -9,12 +9,21 @@
 
 }*/
 
-module "web_security_group" {
+/*module "web_security_group" {
   source = "terraform-aws-modules/security-group/aws//modules/web"
   version = "2.15.0"
 
   # insert the 2 required variables here
 
+    name = "web-security-group"
+    vpc_id = "${module.vpc.vpc_id}"
+}*/
+
+module "security-group_ssh" {
+  source  = "terraform-aws-modules/security-group/aws//modules/ssh"
+  version = "2.15.0"
+
+  # insert the 2 required variables here
     name = "web-security-group"
     vpc_id = "${module.vpc.vpc_id}"
 }
