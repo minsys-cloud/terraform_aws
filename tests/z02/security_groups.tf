aa7642191072097feab30b6f3b70f5722419a97c
lvi <lvi@minsys.io>
2019-03-08 22:16:25 -0500

    updated(test2)  - security groups

--- tests/z02/security_groups.tf
+++ tests/z02/security_groups.tf
@@ -3,4 +3,17 @@ module "security-group" {
   version = "2.15.0"
 
   # insert the 2 required variables here
+
+  name = "security-group"
+  vpc_id = "${module.vpc.vpc_id}"
+
+}
+
+module "web_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/web"
+
+  # insert the 2 required variables here
+
+    name = "web-security-group"
+    vpc_id = "${module.vpc.vpc_id}"
 }
