385c4796410e79182dbfea97ec6e027d71cdde6f
lvi <lvi@minsys.io>
2019-03-09 02:52:13 -0500

    updated(test2) - security groups

--- tests/z02/security_groups.tf
+++ tests/z02/security_groups.tf
@@ -53,3 +53,16 @@ module "security-group_http-80" {
 
   # insert the 2 required variables here
 }
+
+module "security-group_ssh" {
+  source  = "terraform-aws-modules/security-group/aws//modules/ssh"
+  version = "2.15.0"
+
+  # insert the 2 required variables here
+
+  name        = "ssh-sg"
+  description = "Security group with SSH ports open for everybody (IPv4 CIDR), egress ports are all world open"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
