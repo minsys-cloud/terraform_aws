89ae11079dc685a48b837f2542547f68ad899808
lvi <lvi@minsys.io>
2019-03-10 20:14:16 -0400

    updated(test3) - refactoring + output files

--- tests/z03/datas.tf
+++ tests/z03/datas.tf
@@ -1,4 +1,9 @@
 
+
+data "aws_availability_zones" "available" {}
+# Usage -- 
+# aws_avail_zones="${slice(data.aws_availability_zones.available.names,0,2)}"
+
 data "aws_ami" "ubuntu_xenial" {
   most_recent = true
 
@@ -9,6 +14,55 @@ data "aws_ami" "ubuntu_xenial" {
 
   owners      = ["099720109477"]
 }
+
+
+#############################################################
+# Data sources to generate SSH config file & Ansible inventory
+#############################################################
+
+    
+/*
+* Ansible default inventory file
+*/
+
+/*
+data "template_file" "inventory" {
+    template = "${file("${path.module}/templates/inventory.tpl")}"
+
+    vars {
+        public_ip_address_bastion = "${join("\n",formatlist("%s ansible_host=%s" , aws_instance.bastion-server.*.tags.Name, aws_instance.bastion-server.*.public_ip))}"
+        connection_strings_master = "${join("\n",formatlist("%s ansible_host=%s",aws_instance.master.*.tags.Name, aws_instance.master.*.private_ip))}"
+        connection_strings_node = "${join("\n", formatlist("%s ansible_host=%s", aws_instance.worker.*.tags.Name, aws_instance.worker.*.private_ip))}"
+        list_master = "${join("\n",aws_instance.master.*.tags.Name)}"
+        list_node = "${join("\n",aws_instance.worker.*.tags.Name)}"
+        cloud_provider_region = "${var.AWS_DEFAULT_REGION}"
+        cloud_provider_name = "${var.cloud_provider_name}"
+        cloud_provider_shortname = "${var.cloud_provider_shortname}"
+        environment_name = "${var.environment_name}"
+        environment_shortname = "${var.environment_shortname}"
+        ansible_user = "${var.default_user}"
+    }
+}
+*/
+
+/*
+* SSH Config default file
+*/
+
+/*
+data "template_file" "ssh_config" {
+    template = "${file("${path.module}/templates/ssh-bastion.config.tpl")}"
+
+    vars {
+        public_ip_address_bastion = "${aws_instance.bastion-server.0.public_ip}"
+        list_master = "${join(" ", aws_instance.master.*.private_ip)}"
+        list_node = "${join(" ", aws_instance.worker.*.private_ip)}"
+        default_user = "${var.default_user}"
+    }
+}
+*/
+
+
 
 
 #############################################################
--- tests/z03/main.tf
+++ tests/z03/main.tf
@@ -11,3 +11,37 @@ resource "aws_key_pair" "authorized_key" {
   key_name   = "lvi_rsa"
   public_key = "${var.authorized_key_lvi}"
 }
+
+
+    
+/*
+* Ansible default inventory file 
+*/
+
+/*
+
+resource "null_resource" "inventories" {
+  provisioner "local-exec" {
+      command = "echo '${data.template_file.inventory.rendered}' > ansible_inventory"
+  }
+
+  triggers {
+      template = "${data.template_file.inventory.rendered}"
+  }
+}
+*/
+/*
+* SSH Config default file
+*/
+/*
+
+resource "null_resource" "ssh_config" {
+  provisioner "local-exec" {
+      command = "echo '${data.template_file.ssh_config.rendered}' > ssh-bastion.config"
+  }
+
+  triggers {
+      template = "${data.template_file.ssh_config.rendered}"
+  }
+}
+*/
